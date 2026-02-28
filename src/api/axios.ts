a45fcec62563e3fa17a22c7dce5046fb6b4534c4
omerfarooq187 <umarfarooqm871@gmail.com>
2026-02-28 13:49:15 +0500

    first commit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.50:8080",
+  baseURL: "https://api.offhostel.org",
   headers: {
     "Content-Type": "application/json"
   }
